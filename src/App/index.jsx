e0ab1d409b4226c6fc57c11e3c6db8895ff22f6e
Bogdan Buescu <bbuescu@pentalog.com>
2022-08-15 16:41:26 +0200

    - first session

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,31 +1,51 @@
-import { useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Content from "../components/Content";
 import Header from "../components/Header";
 let renderCount = 1;
 
 function App() {
-    console.log(`App re-render count: ${renderCount}`);
-    renderCount++;
+  console.log(`App re-render count: ${renderCount}`);
+  renderCount++;
 
-  const [state, setState] = useState(true);
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const [state, setState] = useState();
+  const [currentDate, setCurrentDate] = useState();
+  const pRef = useRef(null);
 
-  const haderContent = () => {
-    return (
-      <p className="header-paragraph">
-        This is my header child {currentDate.toISOString()}
-      </p>
-    );
-  }
+  useEffect(() => {
+    const oPClick = () => {
+      console.log("paragraph chaged", pRef.current);
+    };
+    pRef.current.addEventListener("click", oPClick);
+
+    return () => {
+        if (pRef.current) {
+            pRef.current.removeEventListener("click", oPClick);
+        }
+    }
+
+  }, [currentDate]);
+
+  // No State
+  // All State
+  // Some state
+
+  const onStateChange = () => {
+    setState(!state);
+  };
+  const onDateChange = () => {
+    setCurrentDate(new Date());
+  };
 
   return (
     <>
       <Header key="header" className="app-header">
-        {haderContent}
+        <p className="header-paragraph">
+          This is my header child {currentDate?.toISOString?.()}
+        </p>
       </Header>
-      <button onClick={() => setState(!state)}>Toggle content</button>
-      <button onClick={() => setCurrentDate(new Date())}>Set current date</button>
-      {state && <Content />}
+
+      <p ref={pRef}>On hover</p>
+      <Content setState={onStateChange} setCurrentDate={onDateChange} />
     </>
   );
 }
--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,5 +1,15 @@
-function Content() {
-    return <div>Content</div>;
+function Content({ setCurrentDate, setState }) {
+  return (
+    <div>
+      Content
+      <div>
+        <button onClick={setState}>Toggle content</button>
+        <button onClick={setCurrentDate}>
+          Set current date
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
