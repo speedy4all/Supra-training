8b66188e8e672dd61ecee43e71ca274934b9bf17
Bogdan Buescu <bbuescu@pentalog.com>
2022-08-16 10:24:10 +0200

    - added fake server - created base structure

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,51 +1,39 @@
+import axios from "axios";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Content from "../components/Content";
 import Header from "../components/Header";
-let renderCount = 1;
+import About from "../containers/About";
+import Form from "../containers/Form";
+import List from "../containers/List";
+const API = "http://localhost:3000";
+
+const fetchUsers = async () => {
+  const resp = await axios.get(`${API}/users`);
+  return resp.data;
+};
+
+const routes = (args = {}) => ({
+  about: <About {...args} />,
+  list: <List {...args} />,
+  form: <Form {...args} />,
+});
 
 function App() {
-  console.log(`App re-render count: ${renderCount}`);
-  renderCount++;
-
-  const [state, setState] = useState();
-  const [currentDate, setCurrentDate] = useState();
-  const pRef = useRef(null);
+  const [path, setPath] = useState("about");
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const oPClick = () => {
-      console.log("paragraph chaged", pRef.current);
-    };
-    pRef.current.addEventListener("click", oPClick);
-
-    return () => {
-        if (pRef.current) {
-            pRef.current.removeEventListener("click", oPClick);
-        }
-    }
-
-  }, [currentDate]);
-
-  // No State
-  // All State
-  // Some state
-
-  const onStateChange = () => {
-    setState(!state);
-  };
-  const onDateChange = () => {
-    setCurrentDate(new Date());
-  };
+    fetchUsers().then((data) => setUsers(data));
+  }, []);
 
   return (
     <>
-      <Header key="header" className="app-header">
-        <p className="header-paragraph">
-          This is my header child {currentDate?.toISOString?.()}
-        </p>
+      <Header className="app-header">
+        <button onClick={() => setPath("about")}>About</button>
+        <button onClick={() => setPath("list")}>List</button>
+        <button onClick={() => setPath("form")}>Form</button>
       </Header>
-
-      <p ref={pRef}>On hover</p>
-      <Content setState={onStateChange} setCurrentDate={onDateChange} />
+      <Content>{routes({ path })[path]}</Content>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,13 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 let renderCount = 1;
 
 function Header({ title = "Supra React course!", children, className }) {
   console.log(`Header re-render count: ${renderCount}`);
   renderCount++;
+
+  useEffect(() => {
+    console.log('Initialize data');
+  }, []);  
 
   return (
     <>
